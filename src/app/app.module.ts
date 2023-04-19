9cfcc9d83a1cd194df4fe8310e3000955f822be6
jhumfreez <25644264+jhumfreez@users.noreply.github.com>
2023-04-19 02:44:31 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, NgxsModuleOptions } from '@ngxs/store';
 import { ZooState } from './store/zoo.state';
 import { NgxsResetPluginModule } from 'ngxs-reset-plugin';
+
+const ngxsConfig: NgxsModuleOptions = {
+  // Checks for mutations in dev environment!
+  developmentMode: true,
+};
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    NgxsModule.forRoot([ZooState]),
+    NgxsModule.forRoot([ZooState], ngxsConfig),
     NgxsResetPluginModule.forRoot(),
   ],
   declarations: [AppComponent],
